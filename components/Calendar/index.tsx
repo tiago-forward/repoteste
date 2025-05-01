95e3bd0f406e98f037f79f2fd738f27018105708
tiago-forward <devtiagoforward@gmail.com>
2025-04-30 22:00:55 -0300

    fix

--- components/Calendar/index.tsx
+++ components/Calendar/index.tsx
@@ -1,12 +1,10 @@
 import { CalendarRoot } from "./CalendarRoot";
-import { CalendarHeader } from "./CalendarHeader";
 import { CalendarTable } from "./CalendarTable";
 import { CalendarTd } from "./CalendarTd";
 import { CalendarButton } from "./CalendarButton";
 
 export const Calendar = {
   Root: CalendarRoot,
-  Header: CalendarHeader,
   Table: CalendarTable,
   Td: CalendarTd,
   Button: CalendarButton,
